375ee495a423f30e1cd07d27b0ba0c68b68f56b4
Brandon Anzaldi <brandon@brandonanzaldi.com>
2014-08-17 22:32:40 -0700

    Documenting a few points in the code.

--- plugins/nsfw/nsfw.php
+++ plugins/nsfw/nsfw.php
@@ -4,7 +4,8 @@ $text = str_replace('/nsfw ','',$text); //remove the "/nsfw " part
 if ($text == "/nsfw") {	
 			$say = " is not safe for work";
 		} else {
-			$say = "[img]" . $text . "#nsfw[/img]";	
+			$say = "[img]" . $text . "#nsfw[/img]";	// This appends the #nsfw fragment to the image, allowing the css rules 
+													// supplied in nsfw.css to catch it, and apply the svg blur filter.
 		}
 $this->insertChatBotMessage( $this->getChannel(), $this->getUserName(). $say );
 	return true; 
--- plugins/soundcloud/soundcloud.php
+++ plugins/soundcloud/soundcloud.php
@@ -1,7 +1,7 @@
 case '/sc':
-    $scapikey = 'YOUR_API_KEY_GOES_HERE';
-    $scUrl = 'http://api.soundcloud.com/resolve.json?url='.urlencode($textParts[1]).'&client_id='.$scapikey;
-    $scJson = json_decode(file_get_contents($scUrl),TRUE);
+    $scapikey = 'YOUR_API_KEY_GOES_HERE'; // You can grab one from http://soundcloud.com/you/apps/new
+    $scUrl = 'http://api.soundcloud.com/resolve.json?url='.urlencode($textParts[1]).'&client_id='.$scapikey; // This is the API endpoint
+    $scJson = json_decode(file_get_contents($scUrl),TRUE); 
     $name = $scJson["user"]["username"]." - ".$scJson["title"];
     $link = $scJson["permalink_url"];
     $named_link = "[url=".$link."]".$name."[/url]";
